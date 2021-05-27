6cc8fae0f203e5e8cd267311fe74963f27013900
Luben.Naumov <luben.naumov@helpline.de>
2021-05-27 19:59:46 +0200

    PoC:v2: Output column types

---
+++ src/StoreLake.Sdk/SqlDom/QueryColumnSourceBase.cs
@@ -0,0 +1,212 @@
+﻿using Microsoft.SqlServer.TransactSql.ScriptDom;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+[assembly: DebuggerDisplay(@"\{Bzz = {BaseIdentifier.Value}}", Target = typeof(SchemaObjectName))]
+[assembly: DebuggerDisplay(@"\{Bzz = {StoreLake.Sdk.SqlDom.SqlDomExtensions.WhatIsThis(SchemaObject)}}", Target = typeof(NamedTableReference))]
+[assembly: DebuggerDisplay(@"{StoreLake.Sdk.SqlDom.SqlDomExtensions.WhatIsThis(this)}", Target = typeof(TSqlFragment))]
+// see [DebuggerTypeProxy(typeof(HashtableDebugView))]
+namespace StoreLake.Sdk.SqlDom
+{
+    internal abstract class QueryColumnBase
+    {
+
+    }
+
+    internal sealed class QueryColumnE : QueryColumnBase
+    {
+        private readonly SelectElement se;
+        public QueryColumnE(SelectElement se)
+        {
+            this.se = se;
+            throw new NotImplementedException(se.WhatIsThis());
+        }
+    }
+    internal abstract class QueryColumnSourceBase
+    {
+        protected string Alias;
+
+        private readonly IDictionary<string, QueryColumnSourceBase> sources = new SortedDictionary<string, QueryColumnSourceBase>();
+        internal readonly List<QueryColumnSourceBase> queries = new List<QueryColumnSourceBase>(); // UNION(s)
+        private readonly List<SelectElement> selectElements = new List<SelectElement>();
+
+        private readonly string _key;
+        public QueryColumnSourceBase(string key)
+        {
+            _key = key;
+        }
+
+        private string Key => _key;
+
+
+        internal static string BuildKey(SchemaObjectName schemaObject, Identifier alias)
+        {
+            if (alias != null)
+            {
+                return alias.Dequote();
+            }
+            else
+            {
+                if (schemaObject.SchemaIdentifier != null)
+                {
+                    return schemaObject.SchemaIdentifier.Dequote() + " . " +
+                           schemaObject.BaseIdentifier.Dequote();
+                }
+                else
+                {
+                    return schemaObject.BaseIdentifier.Dequote();
+                }
+            }
+        }
+        internal static string BuildKey(Identifier itemName, Identifier alias)
+        {
+            if (alias != null)
+            {
+                return alias.Dequote();
+            }
+            else
+            {
+                return itemName.Dequote();
+            }
+        }
+
+        internal void AddColumnSource(QueryColumnSourceBase source)
+        {
+            sources.Add(source.Key, source);
+        }
+
+        public void AddQuery(QueryColumnSourceBase expr)
+        {
+            queries.Add(expr);
+        }
+
+        public QueryColumnSourceBase SetAlias(Identifier alias)
+        {
+            Alias = alias == null ? null : alias.Dequote();
+            return this;
+        }
+
+        public void AddSelectElement(SelectElement ses)
+        {
+        }
+    }
+
+    internal sealed class QueryColumnSourceMQE : QueryColumnSourceBase
+    {
+        // ??? SelectElements => from first + UNION the others
+        public QueryColumnSourceMQE(Identifier alias)
+            : base("$$$_mque_$$$")
+        {
+            SetAlias(alias);
+        }
+    }
+
+
+    [DebuggerDisplay("{DebuggerText}")]
+    internal sealed class QueryColumnSourceNT : QueryColumnSourceBase
+    {
+        internal readonly NamedTableReference NtRef;
+        internal readonly string SchemaName;
+        internal readonly string TableName;
+        public QueryColumnSourceNT(NamedTableReference ntRef)
+            : base(BuildKey(ntRef.SchemaObject, ntRef.Alias))
+        {
+            NtRef = ntRef;
+            SchemaName = NtRef.SchemaObject.SchemaIdentifier != null
+                ? NtRef.SchemaObject.SchemaIdentifier.Dequote()
+                : null;
+            TableName = NtRef.SchemaObject.BaseIdentifier.Dequote();
+            SetAlias(ntRef.Alias);
+        }
+        private string DebuggerText
+        {
+            get
+            {
+                return (SchemaName == null ? "" : "[" + SchemaName + "]")
+                    + "[" + TableName + "]"
+                    + (Alias == null ? "" : " AS [" + Alias + "]");
+            }
+        }
+    }
+
+    [DebuggerDisplay("CTE: {DebuggerText}")]
+    internal sealed class QueryColumnSourceCTE : QueryColumnSourceBase
+    {
+        internal readonly CommonTableExpression NtRef;
+        internal readonly string CteName;
+        public QueryColumnSourceCTE(CommonTableExpression ntRef, Identifier alias)
+            : base(BuildKey(ntRef.ExpressionName, alias))
+        {
+            NtRef = ntRef;
+            CteName = NtRef.ExpressionName.Dequote();
+            SetAlias(alias);
+        }
+
+        private string DebuggerText
+        {
+            get
+            {
+                return CteName
+                    + (Alias == null ? "" : " AS [" + Alias + "]");
+            }
+        }
+
+        internal void AddCteSource(QueryColumnSourceBase ts)
+        {
+            base.AddColumnSource(ts);
+        }
+    }
+
+    [DebuggerDisplay("UDTF:{DebuggerText}")]
+    internal sealed class QueryColumnSourceUDTF : QueryColumnSourceBase
+    {
+        internal readonly SchemaObjectFunctionTableReference Udtf;
+        internal readonly string SchemaName;
+        internal readonly string TableName;
+        public QueryColumnSourceUDTF(SchemaObjectFunctionTableReference udtf)
+            : base(BuildKey(udtf.SchemaObject, udtf.Alias))
+        {
+            Udtf = udtf;
+            SchemaName = udtf.SchemaObject.SchemaIdentifier != null
+                ? udtf.SchemaObject.SchemaIdentifier.Dequote()
+                : null;
+            TableName = udtf.SchemaObject.BaseIdentifier.Dequote();
+            SetAlias(udtf.Alias);
+        }
+
+        private string DebuggerText
+        {
+            get
+            {
+                return (SchemaName == null ? "" : "[" + SchemaName + "]")
+                    + "[" + TableName + "]"
+                    + (Alias == null ? "" : " AS [" + Alias + "]");
+            }
+        }
+    }
+
+    [DebuggerDisplay("VarT: {DebuggerText}")]
+    internal sealed class QueryColumnSourceVarTable : QueryColumnSourceBase
+    {
+        internal readonly VariableTableReference VarTableRef;
+        internal readonly string VariableName;
+        public QueryColumnSourceVarTable(VariableTableReference varTableRef)
+            : base(varTableRef.Alias.Dequote())
+        {
+            VarTableRef = varTableRef;
+            VariableName = varTableRef.Variable.Name;
+            SetAlias(varTableRef.Alias);
+        }
+
+        private string DebuggerText
+        {
+            get
+            {
+                return VariableName
+                    + (Alias == null ? "" : " AS [" + Alias + "]");
+            }
+        }
+    }
+}
--- src/StoreLake.Sdk/SqlDom/StatementOutputColumnTypeResolverV2.cs
+++ src/StoreLake.Sdk/SqlDom/StatementOutputColumnTypeResolverV2.cs
@@ -1,8 +1,21 @@
 ﻿using Microsoft.SqlServer.TransactSql.ScriptDom;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
+[assembly: DebuggerDisplay(@"\{Bzz = {BaseIdentifier.Value}}", Target = typeof(SchemaObjectName))]
+[assembly: DebuggerDisplay(@"\{Bzz = {StoreLake.Sdk.SqlDom.SqlDomExtensions.WhatIsThis(SchemaObject)}}", Target = typeof(NamedTableReference))]
+[assembly: DebuggerDisplay(@"{StoreLake.Sdk.SqlDom.SqlDomExtensions.WhatIsThis(this)}", Target = typeof(TSqlFragment))]
+// see [DebuggerTypeProxy(typeof(HashtableDebugView))]
 namespace StoreLake.Sdk.SqlDom
 {
+    internal sealed class StatementModel
+    {
+        // root: ColumnSource query = 
+        // ColumnSource types(table-view, subquery, udtf:tablefunctions, tablevariablaes, parameters) (Alias, Named, ?)
+        internal QueryColumnSourceBase Root;
+    }
+
     internal class StatementOutputColumnTypeResolverV2
     {
         internal readonly ISchemaMetadataProvider SchemaMetadata;
@@ -25,29 +38,208 @@ namespace StoreLake.Sdk.SqlDom
         internal OutputColumnDescriptor ResolveScalarExpression(SelectScalarExpression node)
         {
             StatementModel model = EnsureModel();
-            throw new NotImplementedException();
+            throw new NotImplementedException(node.WhatIsThis());
         }
 
         private StatementModel EnsureModel()
         {
             if (_model == null)
             {
-                _model = LoadModel();
+                if (statement is SelectStatement stmt_sel)
+                {
+                    _model = QueryModelLoader.LoadModel(stmt_sel.QueryExpression, stmt_sel.WithCtesAndXmlNamespaces);
+                }
+                else
+                {
+                    DataModificationSpecification source_specification = null;
+                    DataModificationStatement stmt_mod = (DataModificationStatement)statement;
+                    if (statement is InsertStatement stmt_ins)
+                    {
+                        source_specification = stmt_ins.InsertSpecification;
+                    }
+                    else if (statement is UpdateStatement stmt_upd)
+                    {
+                        source_specification = stmt_upd.UpdateSpecification;
+                    }
+                    else if (statement is DeleteStatement stmt_del)
+                    {
+                        source_specification = stmt_del.DeleteSpecification;
+                    }
+                    else if (statement is MergeStatement stmt_mrg)
+                    {
+                        source_specification = stmt_mrg.MergeSpecification;
+                    }
+
+                    //_model = QueryModelLoader.LoadModel(source_specification.OutputClause, stmt_mod.WithCtesAndXmlNamespaces);
+                    throw new NotImplementedException(statement.WhatIsThis());
+                }
+
             }
             return _model;
         }
 
-        private StatementModel LoadModel()
-        {
-            StatementModel model = new StatementModel();
-            return model;
-        }
+
     }
 
-    internal sealed class StatementModel
+
+    internal static class QueryModelLoader
     {
-        // root: ColumnSource query = 
-        // ColumnSource types(table-view, subquery, udtf:tablefunctions, tablevariablaes, parameters) (Alias, Named, ?)
+        internal static StatementModel LoadModel(QueryExpression qryExpr, WithCtesAndXmlNamespaces ctes)
+        {
+            StatementModel model = new StatementModel();
+            List<QueryColumnSourceBase> root_sources = new List<QueryColumnSourceBase>();
+            CollectQueryExpression(null, qryExpr, null, ctes, (ts) =>
+            {
+                root_sources.Add(ts);
+            });
+
+            if (root_sources.Count == 1)
+            {
+                model.Root = root_sources[0];
+            }
+            else
+            {
+                var source = new QueryColumnSourceMQE(null);
+                root_sources.ForEach(ts => source.AddColumnSource(ts));
+
+                model.Root = source;
+            }
+            return model;
+        }
+
+        private static void CollectQueryExpression(QueryColumnSourceBase unionFirst, QueryExpression qryExr, Identifier alias, WithCtesAndXmlNamespaces ctes, Action<QueryColumnSourceBase> collector)
+        {
+            if (qryExr is QuerySpecification qrySpec)
+            {
+                var source = new QueryColumnSourceMQE(alias);
+
+                foreach (TableReference tableRef in qrySpec.FromClause.TableReferences)
+                {
+                    CollectTableRef(tableRef, ctes, (ts) =>
+                    {
+                        source.AddColumnSource(ts);
+                    });
+                }
+
+                collector(source);
+
+            }
+            else if (qryExr is BinaryQueryExpression bqExpr)
+            {
+                if (unionFirst == null)
+                {
+                    var source = new QueryColumnSourceMQE(alias);
+                    CollectQueryExpression(source, bqExpr.FirstQueryExpression, alias, ctes, ts =>
+                    {
+                        source.AddColumnSource(ts);
+                    });
+
+                    CollectQueryExpression(source, bqExpr.SecondQueryExpression, alias, ctes, ts =>
+                    {
+                        source.AddQuery(ts);
+                    });
+                    collector(source);
+                }
+                else
+                {
+                    CollectQueryExpression(unionFirst, bqExpr.FirstQueryExpression, alias, ctes, ts =>
+                    {
+                        unionFirst.AddQuery(ts);
+                    });
+
+                    CollectQueryExpression(unionFirst, bqExpr.SecondQueryExpression, alias, ctes, ts =>
+                    {
+                        unionFirst.AddQuery(ts);
+                    });
+                }
+            }
+            else
+            {
+                throw new NotImplementedException(qryExr.WhatIsThis());
+            }
+        }
+
+        private static void CollectTableRef(TableReference tableRef, WithCtesAndXmlNamespaces ctes, Action<QueryColumnSourceBase> collector)
+        {
+            if (tableRef is QualifiedJoin qJoin)
+            {
+                CollectTableRef(qJoin.FirstTableReference, ctes, collector);
+                CollectTableRef(qJoin.SecondTableReference, ctes, collector);
+            }
+            else if (tableRef is UnqualifiedJoin uqJoin)
+            {
+                CollectTableRef(uqJoin.FirstTableReference, ctes, collector);
+                CollectTableRef(uqJoin.SecondTableReference, ctes, collector);
+            }
+            else if (tableRef is NamedTableReference ntRef)
+            {
+                CollectNamedTableReference(ntRef, ctes, collector);
+            }
+            else if (tableRef is SchemaObjectFunctionTableReference udtfRef)
+            {
+                collector(new QueryColumnSourceUDTF(udtfRef));
+            }
+            else if (tableRef is VariableTableReference varTableRef)
+            {
+                collector(new QueryColumnSourceVarTable(varTableRef));
+            }
+            else
+            {
+                throw new NotImplementedException(tableRef.WhatIsThis());
+            }
+        }
+
+        private static void CollectNamedTableReference(NamedTableReference node, WithCtesAndXmlNamespaces ctes, Action<QueryColumnSourceBase> collector)
+        {
+            if (node.SchemaObject.SchemaIdentifier == null &&
+                ctes != null &&
+                TryGetCTEByName(ctes, node.SchemaObject.BaseIdentifier.Dequote(), out CommonTableExpression cte))
+            {
+                var cte_source = new QueryColumnSourceCTE(cte, node.Alias);
+
+                CollectQueryExpression(null, cte.QueryExpression, null, ctes, ts =>
+                {
+                    cte_source.AddCteSource(ts);
+                    //cte_source.AddColumnSource(ts);
+                });
+
+                //foreach (var src in cte_qry_source.sources)
+                //    cte_source.AddColumnSource(src);
+                //foreach (var qry in cte_qry_source.queries)
+                //    cte_source.AddQuery(qry);
+
+                //cte_source.SetColumns(cte_qry_source);
+                //foreach (var col in cte.Columns)
+                //{
+                //    //cte_source.AddSelectElement();
+                //}
+
+                collector(cte_source);
+            }
+            else
+            {
+                collector(new QueryColumnSourceNT(node));
+            }
+
+        }
+
+        private static bool TryGetCTEByName(WithCtesAndXmlNamespaces ctes, string sourceName, out CommonTableExpression result)
+        {
+            foreach (CommonTableExpression cte in ctes.CommonTableExpressions)
+            {
+                string cteName = cte.ExpressionName.Dequote();
+
+                //if (ShouldTrySourceOrAlias(sourceNameOrAlias, cte.ExpressionName))
+                if (string.Equals(sourceName, cteName, StringComparison.OrdinalIgnoreCase))
+                {
+                    result = cte;
+                    return true;
+                }
+            }
+
+            result = null;
+            return false;
+        }
     }
 
 }
